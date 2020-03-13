5c56628fa4452e7c03d736296000d058c21e6274
Haya <yamanakahaya78@icloud.com>
2020-03-13 12:28:42 +0900

    chapter4

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30}
+  validate :validate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, ->{ oreder(created_at: :desc) }
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマは含めることはできません') if name&.include?(',')
+  end
 end
